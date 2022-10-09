cb458ff4fc76319ec2837139c0b6dd9f7f53e151
AbdullahAjayi <hafizajayi2308@gmail.com>
2022-10-09 11:38:55 +0100

    completed nav and sidebar styling

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -8,7 +8,11 @@ import { links } from "../data/dummy";
 import { useStateContext } from "../contexts/ContextProvider";
 
 const Sidebar = () => {
-  const { activeMenu, setActiveMenu } = useStateContext();
+  const { activeMenu, setActiveMenu, screenSize } = useStateContext();
+
+  const handleCloseSideBar = () => {
+    if (screenSize <= 900) setActiveMenu(false);
+  };
 
   const activeLink = "flex items-center gap-5 pl-4 mt-3 pb-2.5 rounded-lg text-white text-md m-2";
   const normalLink =
@@ -22,7 +26,7 @@ const Sidebar = () => {
             <Link
               to="/"
               className="items-center flex gap-3 ml-3 mt-4 font-extrabold text-xl tracking-tight dark:text-white text-slate-900"
-              onClick={() => setActiveMenu(false)}
+              onClick={handleCloseSideBar}
             >
               <SiShopware /> <span>Shoppy</span>
             </Link>
@@ -44,7 +48,7 @@ const Sidebar = () => {
                   <NavLink
                     to={`/${link.name}`}
                     key={link.name}
-                    onClick={() => {}}
+                    onClick={handleCloseSideBar}
                     className={({ isActive }) => (isActive ? activeLink : normalLink)}
                   >
                     {link.icon}
