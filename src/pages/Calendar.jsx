ab1b7bb430743212860db285cef26e77cc0b911a
AbdullahAjayi <hafizajayi2308@gmail.com>
2022-10-12 11:28:57 +0100

    Built calendar page

--- src/pages/Calendar.jsx
+++ src/pages/Calendar.jsx
@@ -1,5 +1,34 @@
+import {
+  ScheduleComponent,
+  ViewsDirective,
+  ViewDirective,
+  Day,
+  Week,
+  WorkWeek,
+  Resize,
+  Month,
+  Agenda,
+  Inject,
+  DragAndDrop,
+} from "@syncfusion/ej2-react-schedule";
+import { DatePickerComponent } from "@syncfusion/ej2-react-calendars";
+
+import { scheduleData } from "../data/dummy";
+import { Header } from "../components";
+
 const Calendar = () => {
-  return <div>Calendar</div>;
+  return (
+    <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
+      <Header category="App" title="Calendar" />
+      <ScheduleComponent
+        eventSettings={{ dataSource: scheduleData }}
+        selectedDate={new Date(2021, 0, 10)}
+        height="650px"
+      >
+        <Inject services={[Day, Week, WorkWeek, Agenda, Resize, DragAndDrop]} />
+      </ScheduleComponent>
+    </div>
+  );
 };
 
 export default Calendar;
