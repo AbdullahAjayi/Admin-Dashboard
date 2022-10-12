28dd494635166c65aa1884680d80bcbb5b3d5b4b
AbdullahAjayi <hafizajayi2308@gmail.com>
2022-10-12 11:23:38 +0100

    built orders page

--- src/pages/Orders.jsx
+++ src/pages/Orders.jsx
@@ -1,5 +1,35 @@
+import {
+  GridComponent,
+  ColumnsDirective,
+  ColumnDirective,
+  Resize,
+  Sort,
+  ContextMenu,
+  Filter,
+  Page,
+  ExcelExport,
+  PdfExport,
+  Edit,
+  Inject,
+} from "@syncfusion/ej2-react-grids";
+
+import { Header } from "../components";
+
+import { ordersData, contextMenuItems, ordersGrid } from "../data/dummy";
+
 const Orders = () => {
-  return <div>Orders</div>;
+  return (
+    <div className="m-2 md:m-10 p-2 md:p-10 mt-24 bg-white rounded-3xl">
+      <Header category="Page" title="Orders" />
+      <GridComponent id="gridcomp" dataSource={ordersData}>
+        <ColumnsDirective>
+          {ordersGrid.map((item, index) => (
+            <ColumnDirective key={index} {...item} />
+          ))}
+        </ColumnsDirective>
+      </GridComponent>
+    </div>
+  );
 };
 
 export default Orders;
